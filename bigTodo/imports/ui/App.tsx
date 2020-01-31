81f40373612485e0fdc69219e54793dbac34deb2
HaQadosch Berraka <haqadosch@pm.me>
2020-01-31 23:45:39 +0000

    lesson 4:  Forms and Events

--- bigTodo/imports/ui/App.tsx
+++ bigTodo/imports/ui/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withTracker } from "meteor/react-meteor-data";
 import { Tasks } from "../api/tasks";
 
@@ -10,14 +10,26 @@ interface IApp {
 
 export interface task {
   _id: number
-  text: string
+  text: string,
+  createdAt: Date
 }
 
 export const App: React.FC<IApp> = ({ tasks }) => {
+  const [inputTask, setInputTask] = useState<string>('')
+
   return (
     <div className="container">
       <header>
         <h1>Todo List</h1>
+
+        <form className="new-task" onSubmit={ handleSubmit }>
+          <input
+            type="text"
+            onChange={ handleChange }
+            value={ inputTask }
+            placeholder="Type to add new task" />
+
+        </form>
       </header>
       <ul>
         { tasks.map(({ _id, text }) => {
@@ -26,8 +38,23 @@ export const App: React.FC<IApp> = ({ tasks }) => {
       </ul>
     </div>
   )
+
+  function handleChange ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void {
+    setInputTask(() => value)
+  }
+
+  function handleSubmit (event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    Tasks.insert({
+      text: inputTask.trim(),
+      createdAt: new Date()
+    })
+
+    setInputTask(() => '')
+  }
 }
 
 export const TrackedApp = withTracker(() => ({
-  tasks: Tasks.find({} as any).fetch()
+  tasks: Tasks.find({} as any, { sort: { createdAt: -1 } }).fetch()
 }))(App)
